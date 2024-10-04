f0272e29e1cb6ef744a26f89a39242fc041517f8
techlabs-be-devgit <techlabs-be-devgit@matchps.com>
2024-10-04 19:50:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,34 @@
 provider "azurerm" {
-subscription_id = "5543a680-faa8-45a5-919e-7fb50b54ee90"
   features {}
 }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.66.0"
+    }
+  }
+  required_version = ">= 0.14"
+}
 
-resource "azurerm_resource_group" "example" {
-  name     = "Sample_TF_resource"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg" {
+  name     = "learnk8sResourceGroup"
+  location = "northeurope"
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "learnk8scluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "learnk8scluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "2"
+    vm_size    = "standard_d2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
