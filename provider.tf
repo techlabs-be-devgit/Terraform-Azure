5b36f5857144510482c80df48e8665b2cdad611b
techlabs-be-devgit <techlabs-be-devgit@matchps.com>
2024-10-04 18:03:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,8 @@
 provider "azurerm" {
-subscription_id = "5543a680-faa8-45a5-919e-7fb50b54ee90"
   features {}
-}
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.66.0"
-    }
-  }
-  required_version = ">= 0.14"
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
