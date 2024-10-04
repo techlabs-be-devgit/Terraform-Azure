b9e3e20f7d0fcdb8d346905e728927c61f6b6a38
techlabs-be-devgit <techlabs-be-devgit@matchps.com>
2024-10-04 20:26:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.66.0"
-    }
-  }
-  required_version = ">= 0.14"
-}
+
 
 resource "azurerm_resource_group" "rg" {
   name     = "learnk8sResourceGroup"
