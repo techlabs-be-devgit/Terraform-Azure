794db1fce0fd3e39442010d4b3104886d9948c2e
techlabs-be-devgit <techlabs-be-devgit@matchps.com>
2024-10-04 15:47:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
-provider "azurerm" {
-subscription_id = "5543a680-faa8-45a5-919e-7fb50b54ee90"
-  features {}
+resource "azurerm_resource_group" "rg" {
+  name     = "AKSwithterraform"
+  location = "northeurope"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "Sample_TF_resource"
-  location = "West Europe"
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "learnk8scluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "learnk8scluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "3"
+    vm_size    = "standard_d2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
